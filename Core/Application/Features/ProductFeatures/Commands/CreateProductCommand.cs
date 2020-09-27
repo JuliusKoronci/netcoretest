505b14b3509a4f6c59d1b161404ba9949179509c
Julius Koronci <julius.koronci@external.erstegroup.com>
2020-09-27 22:35:24 +0200

    first commit

--- Core/Application/Features/ProductFeatures/Commands/CreateProductCommand.cs
+++ Core/Application/Features/ProductFeatures/Commands/CreateProductCommand.cs
@@ -13,25 +13,24 @@ namespace Application.Features.ProductFeatures.Commands
         public string Description { get; set; }
         public decimal Rate { get; set; }
         
-    }
-    
-    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, int>
-    {
-        private readonly IApplicationDbContext _context;
-        public CreateProductCommandHandler(IApplicationDbContext context)
+        public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, int>
         {
-            _context = context;
-        }
-        public async Task<int> Handle(CreateProductCommand command, CancellationToken cancellationToken)
-        {
-            var product = new Product();
-            product.Barcode = command.Barcode;
-            product.Name = command.Name;
-            product.Rate = command.Rate;
-            product.Description = command.Description;
-            _context.Products.Add(product);
-            await _context.SaveChangesAsync();
-            return product.Id;
+            private readonly IApplicationDbContext _context;
+            public CreateProductCommandHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<int> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+            {
+                var product = new Product();
+                product.Barcode = command.Barcode;
+                product.Name = command.Name;
+                product.Rate = command.Rate;
+                product.Description = command.Description;
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+                return product.Id;
+            }
         }
     }
 }
